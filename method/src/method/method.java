aa3baa003e03754169c32c9f837517ac7229be0d
Nao-app <Naoki@192.168.10.102>
2015-02-17 03:31:41 +0900

    メソッドの学習1

---
+++ method/src/method/method.java
@@ -0,0 +1,14 @@
+package method;
+
+public class method {
+
+	public static void main(String[] args) {
+		System.out.println("メソッドを呼び出す");
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました");
+	}
+	public static void hello(){
+		System.out.println("直樹さんこんにちは");
+	}
+
+}

9ab6de7d14b154bb934633ae39ed0ccf86ab453e
Nao-app <Naoki@192.168.10.102>
2015-02-17 07:20:10 +0900

    メソッドの練習(2)

--- Overload/src/overload/Overload.java
+++ Overload/src/overload/Overload.java
@@ -1,12 +1,18 @@
 package overload;
 
 public class Overload{
-	public static void email(String title, String address, String text){
-		System.out.println("『"+ address + "』に以下のメールを送信しました");
-		System.out.println("件名:" + title);
-		System.out.println("本文" + text);
+	public static double clacTriangleArea(double bottom, double height){
+		double Tri = bottom * height; 
+		return Tri;
 	}
-	public static void main(String[] args){
-		email("明日は○◯","XXXX@mail.com","本文を表示");
+	public static double calcCircleArea(double radius){
+		double Cir = radius * radius * 3.14;
+		return Cir;
+	}
+	public static void main (String[] args){
+		double tri = clacTriangleArea(8.5,6.5);
+		double Cir = calcCircleArea(8.2);
+		System.out.println("円の面積" + Cir);
+		System.out.println("三角の面積" +   tri);
 	}
 }

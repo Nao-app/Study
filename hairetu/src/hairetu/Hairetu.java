a69e0857103fff8abbfe210d821cce632f5359ba
Nao-app <Naoki@192.168.10.102>
2015-02-17 03:17:26 +0900

    配列の学習6

--- hairetu/src/hairetu/Hairetu.java
+++ hairetu/src/hairetu/Hairetu.java
@@ -3,10 +3,9 @@ package hairetu;
 public class Hairetu {
 
 	public static void main(String[] args) {
-		int[] score = {20,30,40,50,60,80,};
-		int sum = 0;
-		for(int value : score){
-			System.out.println(value);
+		int[] moneyList = {121902,8302,55100};
+		for(int i = 0;i < moneyList.length; i++){
+			System.out.println("口座残高:" + moneyList[i]);
 		}
 	}
 

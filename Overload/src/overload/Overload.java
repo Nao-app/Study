3e94430c7d7754d3a722c9e8471f7c79028ee150
Nao-app <Naoki@192.168.10.102>
2015-02-17 05:21:26 +0900

    値の引き渡し

--- Overload/src/overload/Overload.java
+++ Overload/src/overload/Overload.java
@@ -1,21 +1,14 @@
 package overload;
 
 public class Overload {
-	//1つ目のaddメソッド
-	public static int add( int x, int y){
-		return x + y;
+	//int型配列を受け取り全ての要素を表示するメソッド
+	public static void printArray(int[] array){
+		for(int element : array){
+			System.out.println(element);
+		}
 	}
-	//2つ目のaddメソッド
-	public static double add(double x, double y){
-		return x + y;
-	}
-	//3つ目のaddメソッド
-	public static String add(String x, String y){
-		return x + y;
-	}
-	public static void main (String[] args){
-		System.out.println(add(10,20));
-		System.out.println(add(3.5,2.7));
-		System.out.println(add("Hello","World"));
+	public static void main(String[] args){
+		int[] array = {1,2,3};
+		printArray(array);
 	}
 }

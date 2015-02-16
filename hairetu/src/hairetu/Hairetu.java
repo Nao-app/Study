75a65d436d65e4b3ab5b06eb61ed45f2268ba63d
Nao-app <Naoki@192.168.10.102>
2015-02-17 02:51:59 +0900

    配列の学習3

--- hairetu/src/hairetu/Hairetu.java
+++ hairetu/src/hairetu/Hairetu.java
@@ -3,10 +3,12 @@ package hairetu;
 public class Hairetu {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		int [] score = new int[5];
-		score[1] = 30;
-		System.out.println(score[1]);
+		int[] score = {20,30,40,50,80};
+		int sum = score[1] + score[2] + score[3] + score[4] + score[0];
+		int avg =sum / score.length;
+		System.out.println("合計点:" + sum);
+		System.out.println("平均点:" + avg);
+		
 	}
 
 }

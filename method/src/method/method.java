986966b36776f2150866c21e1382e74df22eca44
Nao-app <Naoki@192.168.10.102>
2015-02-17 03:35:58 +0900

    メソッドの学習2

--- method/src/method/method.java
+++ method/src/method/method.java
@@ -1,14 +1,14 @@
 package method;
 
 public class method {
-
-	public static void main(String[] args) {
-		System.out.println("メソッドを呼び出す");
-		hello();
-		System.out.println("メソッドの呼び出しが終わりました");
+	public static void methodA(){
+		System.out.println("methodA");
+		methodB();
 	}
-	public static void hello(){
-		System.out.println("直樹さんこんにちは");
+	public static void methodB(){
+		System.out.println("methodB");
 	}
-
+	public static void main (String[] args){
+		methodA();
+	}
 }

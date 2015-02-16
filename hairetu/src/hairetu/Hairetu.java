591d72fc585c68fdd04486be7a464ccd33fbee42
Nao-app <Naoki@192.168.10.102>
2015-02-17 02:47:03 +0900

    配列の学習2

--- hairetu/src/hairetu/Hairetu.java
+++ hairetu/src/hairetu/Hairetu.java
@@ -5,8 +5,8 @@ public class Hairetu {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int [] score = new int[5];
-		int kobakos = score.length;
-		System.out.println("配列の数：" + kobakos);
+		score[1] = 30;
+		System.out.println(score[1]);
 	}
 
 }

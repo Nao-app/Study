3d9cc84eddeec89ab12dc6669d77697357a0fd08
Nao-app <Naoki@192.168.10.102>
2015-02-17 04:46:22 +0900

    メソッドの学習(戻り値2）

--- method/src/method/method.java
+++ method/src/method/method.java
@@ -1,12 +1,11 @@
 package method;
 
 public class method{
-	public static int add (int x, int y){
-		int ans= x+y;
+	public static int add( int x , int y){
+		int ans = x + y;
 		return ans;
 	}
 	public static void main (String[] args){
-		int ans = add(100,10);
-		System.out.println("100 + 10 = " + ans);
+		System.out.println(add (add(10,20) ,add(30,40)));
 	}
 }

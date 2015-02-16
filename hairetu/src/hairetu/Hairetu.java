d1d02585f96bd9e4a9702259c9e2801d791db606
Nao-app <Naoki@192.168.10.102>
2015-02-17 03:01:11 +0900

    配列の学習4 拡張for文

--- hairetu/src/hairetu/Hairetu.java
+++ hairetu/src/hairetu/Hairetu.java
@@ -5,11 +5,9 @@ public class Hairetu {
 	public static void main(String[] args) {
 		int[] score = {20,30,40,50,60,80,};
 		int sum = 0;
-		for(int i = 0; i<score.length; i++){
-			System.out.println(score[i]);
-			sum += score[i];
+		for(int value : score){
+			System.out.println(value);
 		}
-		System.out.println(sum);
 	}
 
 }

4ea8ee06649bb7f2d09c3870020334f199230516
Nao-app <Naoki@192.168.10.102>
2015-02-26 16:20:29 +0900

    Set

--- String/src/string/Main.java
+++ String/src/string/Main.java
@@ -1,16 +1,16 @@
 package string;
 
-import java.util.ArrayList;
-
+import java.util.HashSet;
+import java.util.Set;
 public class Main {
 	public static void main(String[] args){
-		ArrayList<Integer> points = new ArrayList<Integer>();
-		points.add(10);
-		points.add(80);
-		points.add(75);
-		
-		for(int i: points){
-			System.out.println(i);
+		Set<String> Colors = new HashSet<String>();
+		Colors.add("赤");
+		Colors.add("青");
+		Colors.add("黄");
+		System.out.println("色は" + Colors.size() + "種類");
+		for(String s : Colors){
+			System.out.print(s + "→");
 		}
 	}
 }

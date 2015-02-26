475ff842e83bb08789cde95bc8f6a5d72b54fc1f
Nao-app <Naoki@192.168.10.102>
2015-02-26 14:29:54 +0900

    ArrayList

--- String/src/string/Main.java
+++ String/src/string/Main.java
@@ -1,9 +1,16 @@
 package string;
 
+import java.util.ArrayList;
+
 public class Main {
-	String concatPath(String folder, String file){
-		if(!folder.endsWith("¥¥")) folder +="¥¥";
-		return folder + file;
+	public static void main(String[] args){
+		ArrayList<Integer> points = new ArrayList<Integer>();
+		points.add(10);
+		points.add(80);
+		points.add(75);
 		
+		for(int i: points){
+			System.out.println(i);
+		}
 	}
 }
